5122a6a32a318d52cc12aa26f70468b602208cd1
ooriori <seyassrcruzr@gmail.com>
2026-02-08 16:37:42 -0400

    Update README with progress

--- 01-fundamentos-js-y-dom/script.js
+++ 01-fundamentos-js-y-dom/script.js
@@ -7,3 +7,32 @@ let activos = []
 const btnAgregar = documnt.getElementById('btn-agregar');
 const filtroCate = document.getElementById('filtro-categoria')
 const output = document.getElementById ('output')
+
+// funcion para agregar datos
+btnAgregar.addEventListener('click', () =>
+    {
+        const nombre = document.getElementById('nombre').value;
+        const  categoria = document.getElementById('categoria').value;
+        const valor = Number(document.getElementById)('valor').value;
+
+// logica de validacion (Integridad de datos)
+
+            if (nombre.trim() === "" || valor <= 0) {
+                alert("Error: Por favor ingresa datos validos para el archivo");
+                return; 
+            }
+
+            
+            const nuevoActivo = {
+    id: Date.now(), // generamos un activo unico simple
+    nombre,
+    categoria,
+    valor
+    };
+
+            activos.push(nuevoActivo);
+            console.log("Activo registrado:", nuevoActivo);
+            renderizar(activos);
+
+});
+
